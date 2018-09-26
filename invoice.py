3c3ce95fac99ba4f17ac4a5d4b0d0e885652a3b1
Stefan Trost <stefan@stefantrost.co>
2018-09-26 21:20:47 +0200

    Print items and greetings

--- invoice.py
+++ invoice.py
@@ -3,8 +3,10 @@ from fpdf import FPDF
 
 
 class Invoice:
-    def __init__(self):
-        self.pdf = FPDF()
+    def __init__(self, title, name):
+        self.title = title
+        self.name = name
+        self.pdf = PDF()
         self.pdf.set_font('Times', '', 12)
         self.pdf.add_page()
 
@@ -69,7 +71,62 @@ class Invoice:
         self.pdf.set_x(190 - self.pdf.get_string_width(invoice_date))
         self.pdf.write(5, invoice_date)
         self.new_line(4)
-        self.pdf.write(5, "Sehr geehrte Frau Gerigk,\n\nwie vereinbart berechne ich Ihnen für folgende Leistungen den folgenden Betrag:")
+        self.pdf.write(5, f"Sehr geehrte {self.title} {self.name},\n\nwie vereinbart berechne ich Ihnen für folgende Leistungen den folgenden Betrag:")
+        self.new_line(3)
+
+    def invoice_items(self, items_list):
+        for item in items_list:
+            self.pdf.write(5, item['label'])
+            value = f"{item['value']} EUR"
+            self.pdf.set_x(190 - self.pdf.get_string_width(value))
+            self.pdf.write(5, value)
+            self.new_line()
+
+        total = sum([self._parse_formatted_float(item['value']) for item in items_list])
+        y = self.pdf.get_y() + 1
+        self.pdf.line(10, y, 193, y)
+        self.pdf.write(10, "Gesamt:")
+        value = f"{self._format_float(total)} EUR"
+        self.pdf.set_x(190 - self.pdf.get_string_width(value))
+        self.pdf.write(10, value)
+        self.new_line()
+        self.pdf.set_font('Times', '', 9)
+        self.pdf.write(5, "Betrag zahlbar sofort ohne Abzug.")
+        self.pdf.set_font('Times', '', 12)
+        self.new_line(3)
+
+
+    def greetings(self):
+        self.pdf.write(5, "Steuernummer: ")
+        self.new_line()
+        self.pdf.write(5, "Finanzamt: Hofheim a. Ts.")
+        self.new_line(3)
+        self.pdf.write(5, "Vielen Dank für Ihr Vertrauen!")
+        self.new_line(4)
+        self.pdf.write(5, "Mit besten Grüßen")
+        self.new_line(2)
+        self.pdf.write(5, "Stefan Trost")
+
 
     def print_invoice(self):
         self.pdf.output('invoice.pdf')
+
+    def _parse_formatted_float(self, formatted_float: str):
+        return float(formatted_float.replace(',', '.'))
+
+    def _format_float(self, number: float):
+        string = str(round(number, 2))
+        parts = string.split('.')
+        if len(parts[1]) == 1:
+            parts[1] += "0"
+        return ",".join(parts)
+
+
+
+
+class PDF(FPDF):
+    def footer(self):
+        self.set_y(-15)
+        # Select Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        self.write(4, "Bankverbindung: Bank, IBAN: DE00 XXXX XXXX XXXX XXXX XX")
--- invoice_creator.py
+++ invoice_creator.py
@@ -1,9 +1,32 @@
 from invoice import Invoice
 
-my_invoice = Invoice()
-my_invoice.new_line()
-my_invoice.my_address("Stefan Trost", "Schlehenweg 5", "65719 Hofheim", "stefan@stefantrost.co", "0152 - 561 88 357")
-my_invoice.invoice_address("Justso Gmbh\nChristiane Gerigk", "Prauenheimer Landstrasse 95", "60488 Frankfurt a. M.")
-my_invoice.header()
+def main():
+    print("An wen geht die Rechnung?")
+    invoicee_business_name = input("Ggf. Unternehmensname (oder leer lassen): ")
+    invoicee_title = input("Anrede (Frau/Herr): ")
+    invoicee_name = input("Vorname und Nachname: ")
+    invoicee_street_houseno = input("Strasse und Hausnummer: ")
+    invoicee_postcode_city = input("Plz und Ort: ")
 
-my_invoice.print_invoice()
+    invoice_item = input("Was soll abgerechnet werden? ")
+    invoice_value = input("Wieviel soll abgerechnet werden? ")
+
+    if invoicee_business_name:
+        display_name = f"{invoicee_business_name}\n{invoicee_name}"
+    else :
+        display_name = invoicee_name
+    my_invoice = Invoice(invoicee_title, invoicee_name)
+    my_invoice.new_line()
+    my_invoice.my_address("My Name", "Street No 5", "54321 City", "mail@me.com", "0800 555 444 33")
+    my_invoice.invoice_address(display_name, invoicee_street_houseno, invoicee_postcode_city)
+    my_invoice.header()
+
+    my_invoice.invoice_items([{'label': "32 Stunden a 70 EUR/Stunde", 'value': "2240,01"}, {'label': "20 T-Shirts a 20 EUR/Stück", 'value': "400,03"}])
+
+    my_invoice.greetings()
+
+    my_invoice.print_invoice()
+
+
+if __name__ == "__main__":
+    main()
